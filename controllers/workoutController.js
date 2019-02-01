ed17b5e59580c5d59832e92e105273f08feeb3c6
rutterer <rutterer@gmail.com>
2019-01-31 17:35:17 -0800

    fixed issue with workout controller - now pushes workouts to session according to session id param

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -16,7 +16,7 @@ module.exports = {
     db.Workout.create(req.body)
     .then(function (dbWorkout) {
       console.log(dbWorkout)
-      return db.User.findOneAndUpdate({ _id: req.params.id }, { $push: { workouts: dbWorkout._id } }, { new: true })
+      return db.Session.findOneAndUpdate({ _id: req.params.id }, { $push: { workouts: dbWorkout._id } }, { new: true })
     })
       .then(dbWorkout => res.json(dbWorkout))
       .catch(err => res.status(422).json(err));
@@ -25,7 +25,7 @@ module.exports = {
     db.Workout.create(req.body)
     .then(function (dbWorkout) {
       console.log(dbWorkout)
-      return db.User.findOneAndUpdate({ _id: req.params.id }, { $push: { workouts: dbWorkout._id } }, { new: true })
+      return db.Session.findOneAndUpdate({ _id: req.params.id }, { $push: { workouts: dbWorkout._id } }, { new: true })
     })
       .then(dbWorkout => res.json(dbWorkout))
       .catch(err => res.status(422).json(err));
